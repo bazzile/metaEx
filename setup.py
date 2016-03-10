7b11532c6e1453b8cc6e4196f6c918893b1dadd2
lobanovLenovoOFFICE <lobanov@innoter.com>
2016-03-10 22:07:18 +0300

    скомпилирована программа, добавлены поддержка BKA и  SPOT

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    console=['scanner.py'],
+    name='metaEx',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='lobanov',
+    author_email='bazzzzile@gmail.com',
+    description='', requires=['xlwt']
+)
